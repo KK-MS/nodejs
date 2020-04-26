4994108cac483aabaefb2d553b9113fc257df699
KK-MS <35378397+KK-MS@users.noreply.github.com>
2020-04-26 16:12:11 +0200

    package.json: Main file is specified, thus we can use "node ."

--- code/standalone/index.js
+++ code/standalone/index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 var server = http.createServer(function(req, res) {
 res.writeHead(200);
-res.end('Hi everybody!');
+res.end('Hi everybody with node .!');
 });
 server.listen(8080);
