107e3e029a93d42d2a0b131c8dc99c7ef5a29d84
KK-MS <35378397+KK-MS@users.noreply.github.com>
2020-04-26 16:42:43 +0200

    Install nodemon as dev and change start.script to use nodemon instead of node

--- code/standalone/index.js
+++ code/standalone/index.js
@@ -2,6 +2,8 @@ var http = require('http');
 
 var server = http.createServer(function(req, res) {
 res.writeHead(200);
-res.end('Hi everybody with node .!');
+res.write('Hi everybody with electron in package.json.\n');
+res.write('Start.script is change to nodemon, now server need not to be restarted.\n');
+res.end('But we need electron to be start.script?');
 });
 server.listen(8080);
