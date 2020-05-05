1e1c6a9d1b58efcb2647ed43d1e588157d881cfa
KK-MS <35378397+KK-MS@users.noreply.github.com>
2020-05-05 16:50:07 +0200

    Base code. Install express and nodemon

---
+++ code/express_pkg/src/index.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+res.writeHead(200);
+res.write('Hi active everybody with electron in package.json.\n');
+res.write('Start.script is change to nodemon, now server need not to be restarted.\n');
+res.end('But we need electron to be start.script?');
+});
+server.listen(3000);
